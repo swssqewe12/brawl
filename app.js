1ce8170e500dc000eeb443114abc0fe7f69f8407
swssqewe12 <david@callanan.ie>
2017-04-13 16:06:02 +0100

    13-04-17#8
    Changed selected tab from 'Combos' to 'Legends' in exclusive combos page, legend title now title case in exclusive combos page

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ app.get('/c/:w', function(req, res) {
 
 app.get('/c/e/:l', function(req, res) {
 	var l = req.params.l;
-	var dat = {'legend': l, 'weapons': data.exclusive_combos[l]};
+	var dat = {'legend': l, 'data': data.legends.legend[l], 'weapons': data.exclusive_combos[l]};
 	res.render("comboe", dat)
 })
 
